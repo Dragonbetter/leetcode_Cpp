0b342787efa86eb05477cdcd9568faa980c77995
Dragonbetter <1992367282@qq.com>
2024-08-04 16:51:26 +0800

    完成背包问题，打家劫舍问题，股票问题的总结

--- leetcode/leetcode_10_DP.cpp
+++ leetcode/leetcode_10_DP.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0),left(nullptr),right(nullptr){}
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+    TreeNode(int x,TreeNode *left,TreeNode *right):val(x),left(left),right(right){}
+};
+
 
 // 动规五部曲
 // 1.确定dp数组（dp table）以及下标的含义
@@ -388,6 +400,431 @@ class DP_Solution{
         return dp[m][n];        
     }
     // 完全背包理论基础 2024.07.31!!
+    // 每件物品都有无限个，完全背包的物品可以添加多次，故而内循环采用从小到大的遍历顺序
+    // 为什么遍历物品在外层循环，遍历背包容量在内层循环？
+    // 对于纯完全背包，对于一维dp数组来说，其实两个for循环嵌套顺序是无所谓的！
+    // 但是对应的一些变种，对于遍历顺序还是会有要求的
+    void test_CompletePack(){
+        int N,V;
+        cin>>N>>V;
+        vector<int> weight;
+        vector<int> value;
+        for(int i=0;i<N;i++){
+            int w;
+            int v;
+            cin>>w>>v;
+            weight.push_back(w);
+            value.push_back(v);
+        }
+        vector<int> dp(V+1,0);
+        for(int i=0;i<N;i++){
+            for(int j=weight[i];j<=V;j++){
+                // 需要有足够的空间去装这个背包
+                dp[j]=max(dp[j],dp[j-weight[i]+value[i]]);
+            }
+        }
+    }
+    // leetcode 518 零钱兑换II
+    // ==》 完全背包问题，将物品刚好放进去有多少中可能
+    // dp[j] 代表为j时有多少种可能 dp[j] += dp[j-nums[i]]
+    // 组合不强调元素之间的顺序，排序强调元素之间的顺序
+    // 故而对于的循环顺序是物品在外，确保不会出现{1，5}和{5，1}同时存在，因为前面加了1之后，后面就不会重复加，只会出现在最前面
+    // 而如果是物品在内，则{1，5}和{5，1}则可能在不同背包容量下被不同的选择；就是排列数了
+    // dp[0]一定要为1，dp[0] = 1是 递归公式的基础。如果dp[0] = 0 的话，后面所有推导出来的值都是0了。
+    // dp[2]=dp[0]=1 ==>>dp[1],dp[3]=0;
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1,0);
+        dp[0] = 1;
+        for(int i=0;i<coins.size();i++){ // 遍历物品
+            for(int j=coins[i];j<=amount;j++){   // 遍历背包
+                dp[j] += dp[j-coins[i]]; // 递推
+            }
+        }
+        return dp[amount];
+    }
+    // 377 组合总和IV；
+    // 给定由正整数组成而且存在不重复数字的数组，找出和为给定目标正整数的组合的个数
+    // 完全背包问题，求排列
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,0);
+        dp[0] = 1;
+        for(int j=0;j<=target;j++){
+            for(int i=0;i<nums.size();i++){
+                // 需要注意的是测试用例里可能有这两个数相加超过int的存在==>需要避免这种
+                if(j>=nums[i] && dp[j]<INT_MAX-dp[j-nums[i]]) dp[j] +=dp[j-nums[i]];
+            }
+        }
+        return dp[target];
+    }
+    // 322. 零钱兑换 最小的组合数 
+    // 定义dp[i]为凑成总金额最小需要的个数 dp[i] = min{dp[i],dp[i-nums[i]]+1}; dp[0]=1
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,INT_MAX);
+        // 此处必须设置为INT_MAX 因为里面的每个硬币都是数额无线的，则组合的总数本就没有上限；
+        // 设置其他值都有可能会被超过
+        dp[0]=0; // 面额硬币没有0，故而凑成0的则为0 ==》由示例进行定义
+        for(int i=0;i<coins.size();i++){
+            for(int j=coins[i];j<=amount;j++){
+                if (dp[j - coins[i]] != INT_MAX) { // 如果dp[j - coins[i]]是初始值则跳过
+                // 初始值设置的特别大，则不可能会产生该值  ==》 避免INT-MAX+1 产生越界的操作
+                dp[j] = min(dp[j],dp[j-coins[i]]+1);
+                }
+            }
+        }
+        if (dp[amount]==INT_MAX) return -1;
+        return dp[amount];
+    }
+    // leetcode 279 完全平方数
+    // 完全背包问题，可以转化为由1到n对应的完全平方数，由这些数字去填满结果
+    int numSquares(int n){
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+        // 需要注意边界条件，i不能为0，i*i可以等于n，比如1*1=1
+        for(int i=1;i*i<=n;i++){
+            for(int j=i * i;j<=n;j++){ // 起始值需要大于
+                 dp[j] = min(dp[j - i * i] + 1, dp[j]);
+            }
+        }
+        return dp[n];
+    }
+    // 求组合数：动态规划：518.零钱兑换II (opens new window)
+    // 求排列数：动态规划：377. 组合总和 Ⅳ (opens new window)、动态规划：70. 爬楼梯进阶版（完全背包） (opens new window)
+    // 求最小数：动态规划：322. 零钱兑换 (opens new window)、动态规划：279.完全平方数
+    // leetcode 139 单词拆分 2024.08.03
+    /* 
+    非空字符串s和一个包含非空单词的列表wordDict，判定s是否可以被空格拆分为1一个或多个在字典中出现的单词
+    排列问题，是否存在一个排列其对应的组合可以和s一样；
+    完全背包 ==》 不仅字符的个数需要满足，还需要满足匹配的原则，即该字符与字典中的字符的前面几个是一一对应的
+    dp[i]；字符串长度为i的话，dp[i]为true，表示可以拆分为一个或多个在字典中出现得单词
+    如果确定dp[j] 是true，且 [j, i] 这个区间的子串出现在字典里，那么dp[i]一定是true。（j < i ）。
+    所以递推公式是 if([j, i] 这个区间的子串出现在字典里 && dp[j]是true) 那么 dp[i] = true。
+    
+    */ 
+    bool wordBreak(string s,vector<string> &wordDict){
+        unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
+        vector<bool> dp(s.size()+1,false);
+        dp[0] = true;
+        for(int i = 1; i<=s.size();i++ ){ // 先遍历背包
+            for(int j=0;j<i;j++){ // j需要小于背包的容量！！
+                string word = s.substr(j,i-j);
+                 // 获取从j开始，i-j个位置的字符串，如果在字典里，则说明dp[i] = dp[i-j]
+                 if(wordSet.find(word) != wordSet.end() && dp[j]){
+                    dp[i] = true;
+                 }
+            }
+        }
+        return dp[s.size()];
+    } 
+    // 背包问题主要需要集中关注几个内容 2024.08.03
+    /*
+    1. 背包和物品的遍历顺序，以及各自遍历顺序的正序还是倒序
+    2. 遍历时的起点和终点，同时需要关注背包容量与物品之间的关系，需要确保背包的容量大于物品的重量
+    3. 背包问题公式的设置，递推顺序，求组合，求排列，求最小数，
+    4. 背包问题的遍历起点
+    */
+    // 多重背包 ==》 在01背包和完全背包之间，每个背包的数量是有限制的
+    // 解决方法可以基于01背包进行进一步的衍生，主要解决方法即是在不同物品的循环下再加一个循环，表示的是对应物品数量的循环
+    
+    // 198，打家劫舍
+    /*
+    1. dp[i]表示的是考虑下标i（包括i）以内的房屋，最多可以偷窃的金额为dp[i]。
+       ==>所以下标为0，如果设置其为代表第一件房屋，则对应的结果为nums[0]；
+       而如果设置的是nums.size()+1,则对应的第一个应该是0；
+    2. dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+    3. 初始状态，可能不止一个：dp[0]=0, dp[1] = nums[0]
+    3. 遍历顺序：从小到大，
+    */
+    int rob_101(vector<int> &nums){
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        vector<int> dp(nums.size()+1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i=2;i<=nums.size();i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        return dp[nums.size()];
+    }
+    int rob_102(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        vector<int> dp(nums.size()); // 从0开始的！！
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]); // 针对于第一个则是在0和1之间进行取舍
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.size() - 1];
+    }
+    // leetcode 213 打家劫舍II 2024.08.03
+    // 由线性的变成一圈了！！即首尾也是相邻的，则需要剔除该种情况
+    // 对应的即可在两种情况下(不包含首元素，不包含尾元素)取max 
+    int rob_2(vector<int> &nums){
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return nums[0];
+        // end -1[包含尾元素]/-2[不包含尾元素]
+        int result1 = robrange(nums,0,nums.size()-2); // 情况二
+        int result2 = robrange(nums,1,nums.size()-1); // 情况三
+        return max(result1,result2);
+    }
+    int robrange(vector<int> &nums,int start,int end){
+        if(end == start) return nums[end];
+        vector<int> dp(nums.size());
+        // 重新定义起点
+        dp[start] = nums[start];
+        dp[start+1] = max(nums[start],nums[start+1]);
+        for(int i=start+2;i<=end;i++){
+            // 重新定义遍历千点和终点
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[end];
+    }
+    // leetcode 337 打家劫舍III 2024.08.03
+    /*
+    动态规划仍然是准确的，需要更改和确认的是主要是遍历的顺序；
+    父节点和子节点不能够被同时获取，那么可以利用对应的二叉树的顺序公式进行分析；
+    对于树的话，首先就要想到遍历方式，前中后序（深度优先搜索）还是层序遍历（广度优先搜索）。
+    如果抢了当前节点，两个孩子就不能动，如果没抢当前节点，就可以考虑抢左右孩子（注意这里说的是“考虑”）
+    1. 可以暴力递归，直接基于树的结构撰写对应的dp规则，从而获取到对应的结果，但存在重复计算的过程
+    2. 记忆化递推, 前文第一种暴力递归存在的问题是冗余计算，因为当我们计算左右孩子时，需要用到左右孩子的左右孩子，
+    但这个在之前就已经计算过了，故而可以采用一个map去存储这个结果。
+    3. 动态规划设计，
+    
+    */
+    int rob_301(TreeNode* root){
+        // 时间复杂度：O(n^2)，我们计算了root的四个孙子（左右孩子的孩子）为头结点的子树的情况，又计算了root的左右孩子为头结点的子树的情况，计算左右孩子的时候其实又把孙子计算了一遍。
+        if (root == nullptr) return 0;
+        if (root->left == nullptr && root->right == nullptr) return root->val;
+        // 偷父节点
+        int val1 = root->val;
+        if (root->left) val1 += rob_301(root->left->left) + rob_301(root->left->right); // 跳过root->left，相当于不考虑左孩子了
+        if (root->right) val1 += rob_301(root->right->left) + rob_301(root->right->right); // 跳过root->right，相当于不考虑右孩子了
+        // 不偷父节点
+        int val2 = rob_301(root->left) + rob_301(root->right); // 考虑root的左右孩子
+        return max(val1, val2);
+    }
+    // 记忆化递推 ==> 记录中间过程 2024.08.03
+    unordered_map<TreeNode*,int> umap; //记录计算过的结果
+    int rob_302(TreeNode* root){
+        if(root==nullptr) return 0;
+        if(root->left == nullptr && root->right == nullptr) return root->val;
+        if(umap[root]) return umap[root];
+        // 偷父节点
+        int val1 = root->val;
+        if(root->left) val1 += rob_302(root->left->left) + rob_302(root->left->right);
+        if(root->right) val1 += rob_302(root->right->left) + rob_302(root->right->right);
+        // 不偷父节点
+        int val2 = rob_302(root->left) + rob_302(root->right);
+        umap[root] = max(val1,val2);
+        return max(val1,val2);
+    } 
+    // 树形dp ==> 在树上进行状态转移 2024.08.03
+    /*
+    树形DP，即在树上进行递归公式的推导；需要适当的结合树的遍历方式
+    针对于节点而言，只有两个状态，取或不取，故而可以设计一个长度为2的数组，0位代表不取，1位代表取
+    结合递归，并采用后序遍历，则可以实现正确的数组的更新与替代   
+    */
+    vector<int> robTree(TreeNode* cur){
+        if(cur==nullptr) return vector<int>{0,0};
+        vector<int> left = robTree(cur->left);
+        vector<int> right = robTree(cur->right);
+        // 不偷该节点 ==> 则左右节点都可以偷，故而纳入对应的考虑范围
+        int val1 = max(left[0],left[1])+max(right[0],right[1]);
+        // 偷该节点 则不能偷左右节点
+        int val2 = cur->val + left[0] + right[0];
+        return vector<int>{val1,val2};
+    }
+    int rob(TreeNode * root){
+        vector<int> result = robTree(root);
+        return max(result[0],result[1]);
+    }
+    // 【系列专题学习】买卖股票的最佳时机系列I-II-III-IV-含冷冻期-含手续费-总结
+    // leetcode 121 买卖股票的最佳时机 2024.08.04
+    // 贪心做法 ==》 因为全程只有一次买卖，而想要获得最佳收益，只需要右边的最大值减去左边的最小值即可
+    // 具体做法的话，在遍历过程中不断搜寻左边的最小值，同时用当前值进去最小值，并取max，则可以通过遍历的方式获取对应的max
+    int maxProfit(vector<int> & prices){
+        int min_value = INT_MAX,result = 0;
+        for(int i=0;i<prices.size();i++){
+            min_value = min(min_value,prices[i]);
+            result = max(result,prices[i]-min_value);
+        }
+        return result;
+    }
+    // 动态规划的做法==》针对于股票的持有情况进行分析，类似于前文的树形DP，需要记录两个状态01，代表的时持有股票和不持有股票
+    /* 2024.08.04
+    目标是获得最多的现金，故而都应该是基于max 
+    dp[i][0]: 代表在第i天持有股票时的现金，注意，持有股票并不代表时当天买入的股票；其有两种对应的情况
+              第i-1天就持有了，则dp[i][0] = dp[i-1][0] 
+              第i天买入的，即dp[i][0] = -price[i]
+              ==> dp[i][0] = max(dp[i-1][0],-price[i])
+    dp[i][1]：代表在第i天不持有股票时的现金，注意，不持有股票也不代表时当天卖出的股票，其也有两种对应的情况
+              第i-1天就未持有了，则dp[i][1] = dp[i-1][1]
+              第i-1天卖出的，则dp[i][1] = dp[i-1][0] + price[i]
+              dp[i][1] = max(dp[i-1][1],dp[i-1][0] + price[i])
+    初始化条件，即dp[0][0] = -price[0] ,dp[0][1] = 0
+    循环遍历条件即顺序遍历即可，因为对应的i依赖于先前的i-1的结果
+    return dp[i][1];不持有股票时获得价值会更大
+    */
+   int maxProfit(vector<int> & prices){
+        vector<vector<int>> dp(prices.size(),vector<int>(2));
+        dp[0][0] = -prices[0];
+        for(int i=0;i<prices.size();i++){
+            // 依次递推分析
+            dp[i][0] = max(dp[i-1][0],-prices[i]);
+            dp[i][1] = max(dp[i-1][1],dp[i-1][0]+prices[i]);
+        }
+        return dp[prices.size()-1][1];
+   }
+   // leetcode 122 买卖股票的最佳时机II ==》 可以多次买卖
+   /* 2024.08.04
+   贪心做法：==》 局部最优可以转化为全局最优；利润同样是可分解的 
+   当可以同时买卖多次股票的时候，我们需要尽量选择只有正利润的时刻进行交易，避免负利润的影响
+   故而可以首先基于差值计算出正利润，并将其累加，即可获得最终的结果
+   */
+   int maxProfit_II(vector<int> & prices){
+    int result = 0;
+    for(int i=1;i<prices.size();i++){
+        result += max(prices[i]-prices[i-1],0);
+    }
+    return result;
+   }
+   /*
+    动态规划做法：
+      ==》 主要在于递推公式的变化上；
+      当买入股票时，当前时刻的现金需要考虑前期的利润情况，即不再是-prices[i]；而是对应的dp[i-1][1]-prices[i];
+      需要将前一时刻不持有股票的利润对应的也加进去
+   */
+    int maxProfit_II(vector<int> &prices){
+        vector<vector<int>> dp(prices.size(),vector<int>(2));
+        dp[0][0] = -prices[0];
+        for(int i=1;i<prices.size();i++){
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1]-prices[i]);
+            dp[i][1] = max(dp[i-1][1],dp[i-1][0]+prices[i]);
+        }
+        return dp[prices.size()-1][1];
+    }
+    // leetcode 123 买卖股票的最佳时机III ==> 最多可以完成两笔交易
+    // ==》 可以买卖一次，可以买卖两次，甚至我可以不买卖！！
+    /* 2024.08.04
+    只有一次交易的时候，对应的结果是两个状态，而当对应有两笔交易的时候；
+    可以同步的进行扩展为四个状态，即dp[i][0],dp[i][1],dp[i][2],dp[i][3]
+    分别表示的是第一次持有，第一次不持有，第二次持有，第二次不持有的一个状态
+    而后可以基于此进行状态转换的递推方程的撰写
+    */
+    int maxProfit_III(vector<int> &prices){
+        vector<vector<int>> dp(prices.size(),vector<int>(4));
+        dp[0][0] = -prices[0]; // 第一次持有
+        dp[0][1] = 0; // 第一次不持有
+        dp[0][2] = -prices[0]; // 第二次持有 =》 在第一次买入卖出的基础上进行分析
+        dp[0][3] = 0; // 第二次不持有
+        for(int i=1;i<prices.size();i++){
+            dp[i][0] = max(dp[i-1][0],-prices[i]); // 第一次买股票，则前面并无累计的金额
+            dp[i][1] = max(dp[i-1][1],dp[i-1][0]+prices[i]);
+            dp[i][2] = max(dp[i-1][2],dp[i-1][1]-prices[i]);
+            dp[i][3] = max(dp[i-1][3],dp[i-1][2]+prices[i]);
+        }
+        return dp[prices.size()-1][3];
+    }
+    // leetcode 188 买卖股票的最佳时机IV ==> 最多可以完成k次交易
+    /* 2024.08.04
+    沿用I-III的思路，从1次交易，扩展到2次交易，继续扩展到k次交易，以及无数次交易
+    那么基于k次交易则可以采用与前面类似的方法设计二维数组，即dp[i][j]；其中j则代表买入卖出等的一些状态
+    ==》 那么基于先前的参考可以转换为二层for循环的形式去填充数组。
+    ==》 而同时为了使得数组的形式一直，引入dp[i][0] =dp[i-1][0] 表示无操作。
+    即0(无操作)-1(买入)-2(卖出)-3(买入)-4(卖出)！！
+    */
+    int maxProfit_IV(vector<int> &prices,int k){
+        vector<vector<int>> dp(prices.size(),vector<int>(2*k+1));
+        // 初始化
+        // 奇数代表的是持有，而持有状态下的结果为-prices[0]
+        for(int i=0;i<k;i++){
+            dp[0][2*i+1] = -prices[0]; // 所有的持有状态都是-prices[0]，主要因为是dp[0]==》还未遍历到后面的i
+        }
+        // 遍历结构
+        for(int j=1;j<prices.size();j++){
+            // 注意的是需要确保遍历从1开始 因为dp[j][0]的值是固定的为0
+            for(int i=1;i<=k;i++){ 
+                // 遍历的范围: 1->k ; 0->k-1 
+                // 第2*i+1代表的是持有，则max = 等于前一次持有，与当前此不持有-买出
+                dp[j][2*i-1]=max(dp[j-1][2*i-1],dp[j-1][2*i-2]-prices[j]);
+                // 第2*i代表的是不持有，则max = 等于前一次不持有，与当前次持有+卖出
+                dp[j][2*i] =max(dp[j-1][2*i],dp[j-1][2*i-1]+prices[j]);
+                
+            }
+        }
+        return dp[prices.size()-1][2*k];
+    }
+    // leetcode 309 包含冷冻期 
+    /* 2024.08.04
+    包含冷冻期的话，应该对应的是买入卖出的递推公式需要往前修改？
+    原有的
+    dp[i][0] = max(dp[i-1][0],dp[i-1][1]-prices[i]);
+    dp[i][1] = max(dp[i-1][1],dp[i-1][0]+prices[i]);
+    ==> 需要修改的主要是i-1，即从上一次不持有到这次持有需要隔一天；而从持有到不持有没有限制
+    ==> 可能还是存在问题，需要直接将冷冻期也作为一个状态加入，进行设计
+    动态规划的本质还是一个复杂的状态转移过程，明确状态设计，明确各个状态之间的转移过程即可设计出合适的代码
+    本题状态包含，
+    0.股票持有状态（即是前一天就持有，也是可能今天刚买入(也分为两种情况)）
+    1.股票不持有状态（即是前一天就不持有，也可能是前一天为冷冻期）
+    2.今日售出状态 （即由股票持有转变为不持有）
+    3.冷冻期状态 （由今日售出状态转变过来）
+    状态转移方程设计：==> 需要前期思考明确！！！
+    dp[i][0] = max(dp[i-1][0],dp[i-1][3]-prices[i],dp[i-1][1]-prices[i])
+    dp[i][1] = max(dp[i-1][1],dp[i-1][3])
+    dp[i][2] = dp[i-1][0] + price[i]
+    dp[i][3] = dp[i-1][2]
+    ==>初始化设计，股票持有状态为-prices[0]；其他仍然为0
+    遍历顺序不变，仍然由左向右
+    */
+    int maxProfit_withcoldtime(vector<int> &prices){
+        int n = prices.size();
+        if (n == 0) return 0;
+        vector<vector<int>> dp(prices.size(),vector<int>(4));
+        dp[0][0] = -prices[0];
+        for(int i =1;i<prices.size();i++){
+            // max函数主要用于两个内容的比较 
+            // 状态的设计很为重要！！
+            dp[i][0] = max(dp[i-1][0],max(dp[i-1][3]-prices[i],dp[i-1][1]-prices[i]));
+            dp[i][1] = max(dp[i-1][1],dp[i-1][3]);
+            dp[i][2] = dp[i-1][0] + prices[i];
+            dp[i][3] = dp[i-1][2];
+        }
+        return max(dp[prices.size()-1][3],max(dp[prices.size()-1][2],dp[prices.size()-1][1]));
+    }
+    // leetcode 714 买卖股票的最佳时机 含手续费
+    /*
+    区别就是这里需要多一个减去手续费的操作    
+    */
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] -= prices[0]; // 持股票
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+        }
+        return max(dp[n - 1][0], dp[n - 1][1]);
+    }
+    /*
+    股票问题总结篇：
+    从买卖一次到买卖多次，从最多买卖两次到最多买卖k次，
+    从冷冻期再到手续费，
+    */
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
